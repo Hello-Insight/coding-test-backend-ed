113200a0945a975f3b6c537eb124af54082be3e1
Edgar Nova <ragnarok540@gmail.com>
2019-05-03 20:53:06 -0500

    login and logout

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 import json
 import sqlite3
-from flask import Flask, request, g
+from flask import Flask, session, request, g
 from user import User
 app = Flask(__name__)
+app.secret_key = 's3cr3t_k3y!!!'
 
 DATABASE = 'src/database.db'
 
@@ -33,7 +34,6 @@ def index():
 
 @app.route('/user', methods = ['POST'])
 def postUser():
-
     content = request.get_json()
     update = True
     id_user = 0
@@ -51,18 +51,17 @@ def postUser():
                    content["password"], 
                    content["first_name"], 
                    content["last_name"]) 
-        query_db(usr.update_sql(), usr.update_user_tuple())
+        query_db(usr.update_sql(), usr.update_tuple())
         id_user = str(content["id"])
         status = "200"
         message = "The user was updated successfully"
     else:
-        usr = User("", 
-                   content["email"], 
+        usr = User("", content["email"], 
                    content["password"], 
                    content["first_name"], 
                    content["last_name"]) 
-        query_db(usr.insert_sql(), usr.insert_user_tuple())
-        q = query_db("select max(ID) 'm' from main.USER", (), True)
+        query_db(usr.insert_sql(), usr.insert_tuple())
+        q = query_db(usr.max_sql(), (), True)
         id_user = str(q["m"])
         status = "200"
         message = "The user was created successfully"
@@ -71,6 +70,44 @@ def postUser():
         "status": status,
         "message": message,
         "id": id_user
+    }
+
+    return json.dumps(result)
+
+@app.route('/user/login', methods = ['POST'])
+def login():
+    content = request.get_json()
+    status = "401"
+    message = "Please check your credentials"
+    usr = User("", content["email"], 
+               content["password"], "", "")
+    q = query_db(usr.login_sql(), usr.login_tuple(), True)
+
+    try:
+        pw_hash = q["pw_hash"]
+        if usr.check_pw(pw_hash):
+            session["first"] = q["first"]
+            session["last"] = q["last"]
+            status = "200"
+            message = "User logged in successfully"
+    except TypeError:
+        None
+
+    result = {
+        "status": status,
+        "message": message
+    }
+
+    return json.dumps(result)
+
+@app.route('/user/logout', methods = ['POST'])
+def logout():
+    session.pop('first', None)
+    session.pop('last', None)
+
+    result = {
+        "status": "200",
+        "message": "User logged out successfully"
     }
 
     return json.dumps(result)
--- src/user.py
+++ src/user.py
@@ -9,16 +9,28 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
 
+    def max_sql(self):
+        return "select max(ID) 'm' from main.USER"
+
     def insert_sql(self):
         return 'insert into main.USER (EMAIL, PASSWORD_HASH, FIRST_NAME, LAST_NAME) values (?, ?, ?, ?)'
 
-    def insert_user_tuple(self):
+    def insert_tuple(self):
         hashed = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
         return (self.email, hashed, self.first_name, self.last_name)
 
     def update_sql(self):
         return 'update main.USER set EMAIL = ?, PASSWORD_HASH = ?, FIRST_NAME = ?, LAST_NAME = ? where ID = ?'
 
-    def update_user_tuple(self):
-        hashed = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
-        return (self.email, hashed, self.first_name, self.last_name, self.id_user)
+    def update_tuple(self):
+        pw_hash = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
+        return (self.email, pw_hash, self.first_name, self.last_name, self.id_user)
+
+    def login_sql(self):
+        return "select PASSWORD_HASH 'pw_hash', FIRST_NAME 'first', LAST_NAME 'last' from main.USER where EMAIL = ?"
+
+    def login_tuple(self):
+        return (self.email, )
+
+    def check_pw(self, pw_hash):
+        return bcrypt.checkpw(self.password.encode('utf-8'), pw_hash)
